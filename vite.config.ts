4183f70301c2dda2287a72b4eb70a8152c0e3e42
Imenloader <37957194+Imenloader@users.noreply.github.com>
2026-04-14 09:06:28 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [tailwindcss(), cloudflare()],
+    plugins: [tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
